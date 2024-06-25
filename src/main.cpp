ee4636277438a7f39c2c2c063b801951163a3142
Γιώργος Τριγωνάκης <gtrig84@gmail.com>
2024-06-25 20:58:32 +0300

    Cleanup Chinese

--- src/main.cpp
+++ src/main.cpp
@@ -34,48 +34,45 @@ public:
     {
       auto cfg = _bus_instance.config();
 
-      // SPIバスの設定
-      cfg.spi_host = SPI; // 使用するSPIを選択  ESP32-S2,C3 : SPI2_HOST or SPI3_HOST / ESP32 : VSPI_HOST or HSPI_HOST
-      // ※ ESP-IDFバージョンアップに伴い、VSPI_HOST , HSPI_HOSTの記述は非推奨になるため、エラーが出る場合は代わりにSPI2_HOST , SPI3_HOSTを使用してください。
-      cfg.spi_mode = 0;                  // SPI通信モードを設定 (0 ~ 3)
-      cfg.freq_write = 80000000;         // 传输时的SPI时钟（最高80MHz，四舍五入为80MHz除以整数得到的值）
-      cfg.freq_read = 20000000;          // 接收时的SPI时钟
-      cfg.spi_3wire = true;              // 受信をMOSIピンで行う場合はtrueを設定
-      cfg.use_lock = true;               // 使用事务锁时设置为 true
-      cfg.dma_channel = SPI_DMA_CH_AUTO; // 使用するDMAチャンネルを設定 (0=DMA不使用 / 1=1ch / 2=ch / SPI_DMA_CH_AUTO=自動設定)
-      // ※ ESP-IDFバージョンアップに伴い、DMAチャンネルはSPI_DMA_CH_AUTO(自動設定)が推奨になりました。1ch,2chの指定は非推奨になります。
-      cfg.pin_sclk = SCLK; // SPIのSCLKピン番号を設定
-      cfg.pin_mosi = MOSI; // SPIのCLKピン番号を設定
-      cfg.pin_miso = MISO; // SPIのMISOピン番号を設定 (-1 = disable)
-      cfg.pin_dc = DC;     // SPIのD/Cピン番号を設定  (-1 = disable)
+      cfg.spi_host = SPI; 
+      
+      cfg.spi_mode = 0;                  
+      cfg.freq_write = 80000000;         
+      cfg.freq_read = 20000000;          
+      cfg.spi_3wire = true;              
+      cfg.use_lock = true;               
+      cfg.dma_channel = SPI_DMA_CH_AUTO; 
+      
+      cfg.pin_sclk = SCLK; 
+      cfg.pin_mosi = MOSI; 
+      cfg.pin_miso = MISO; 
+      cfg.pin_dc = DC;     
 
-      _bus_instance.config(cfg);              // 設定値をバスに反映します。
-      _panel_instance.setBus(&_bus_instance); // バスをパネルにセットします。
+      _bus_instance.config(cfg);
+      _panel_instance.setBus(&_bus_instance);
     }
 
-    {                                      // 表示パネル制御の設定を行います。
-      auto cfg = _panel_instance.config(); // 表示パネル設定用の構造体を取得します。
+    {
+      auto cfg = _panel_instance.config();
 
-      cfg.pin_cs = CS;   // CSが接続されているピン番号   (-1 = disable)
-      cfg.pin_rst = RST; // RSTが接続されているピン番号  (-1 = disable)
-      cfg.pin_busy = -1; // BUSYが接続されているピン番号 (-1 = disable)
+      cfg.pin_cs = CS;
+      cfg.pin_rst = RST; 
+      cfg.pin_busy = -1; 
 
-      // ※ 以下の設定値はパネル毎に一般的な初期値が設定さ BUSYが接続されているピン番号 (-1 = disable)れていますので、不明な項目はコメントアウトして試してみてください。
-
-      cfg.memory_width = WIDTH;   // ドライバICがサポートしている最大の幅
-      cfg.memory_height = HEIGHT; // ドライバICがサポートしている最大の高さ
-      cfg.panel_width = WIDTH;    // 実際に表示可能な幅
-      cfg.panel_height = HEIGHT;  // 実際に表示可能な高さ
-      cfg.offset_x = OFFSET_X;    // パネルのX方向オフセット量
-      cfg.offset_y = OFFSET_Y;    // パネルのY方向オフセット量
-      cfg.offset_rotation = 0;    // 值在旋转方向的偏移0~7（4~7是倒置的）
-      cfg.dummy_read_pixel = 8;   // 在读取像素之前读取的虚拟位数
-      cfg.dummy_read_bits = 1;    // 读取像素以外的数据之前的虚拟读取位数
-      cfg.readable = false;       // 如果可以读取数据，则设置为 true
-      cfg.invert = true;          // 如果面板的明暗反转，则设置为 true
-      cfg.rgb_order = RGB_ORDER;  // 如果面板的红色和蓝色被交换，则设置为 true
-      cfg.dlen_16bit = false;     // 对于以 16 位单位发送数据长度的面板，设置为 true
-      cfg.bus_shared = false;     // 如果总线与 SD 卡共享，则设置为 true（使用 drawJpgFile 等执行总线控制）
+      cfg.memory_width = WIDTH;
+      cfg.memory_height = HEIGHT;
+      cfg.panel_width = WIDTH;
+      cfg.panel_height = HEIGHT;
+      cfg.offset_x = OFFSET_X;
+      cfg.offset_y = OFFSET_Y;
+      cfg.offset_rotation = 0;
+      cfg.dummy_read_pixel = 8;
+      cfg.dummy_read_bits = 1;
+      cfg.readable = false;
+      cfg.invert = true;
+      cfg.rgb_order = RGB_ORDER;
+      cfg.dlen_16bit = false;
+      cfg.bus_shared = false;
 
       _panel_instance.config(cfg);
     }
@@ -92,28 +89,28 @@ public:
       _panel_instance.setLight(&_light_instance); // Sets the backlight to the panel.
     }
 
-    { // タッチスクリーン制御の設定を行います。（必要なければ削除）
+    {
       auto cfg = _touch_instance.config();
 
-      cfg.x_min = 0;        // タッチスクリーンから得られる最小のX値(生の値)
-      cfg.x_max = 240;      // タッチスクリーンから得られる最大のX値(生の値)
-      cfg.y_min = 0;        // タッチスクリーンから得られる最小のY値(生の値)
-      cfg.y_max = 240;      // タッチスクリーンから得られる最大のY値(生の値)
-      cfg.pin_int = TP_INT; // INTが接続されているピン番号
+      cfg.x_min = 0;        
+      cfg.x_max = 240;      
+      cfg.y_min = 0;        
+      cfg.y_max = 240;      
+      cfg.pin_int = TP_INT; 
       // cfg.pin_rst = TP_RST;
-      cfg.bus_shared = false;  // 画面と共通のバスを使用している場合 trueを設定
-      cfg.offset_rotation = 0; // 表示とタッチの向きのが一致しない場合の調整 0~7の値で設定
-      cfg.i2c_port = 0;        // 使用するI2Cを選択 (0 or 1)
-      cfg.i2c_addr = 0x15;     // I2Cデバイスアドレス番号
-      cfg.pin_sda = I2C_SDA;   // SDAが接続されているピン番号
-      cfg.pin_scl = I2C_SCL;   // SCLが接続されているピン番号
-      cfg.freq = 400000;       // I2Cクロックを設定
+      cfg.bus_shared = false;
+      cfg.offset_rotation = 0;
+      cfg.i2c_port = 0;
+      cfg.i2c_addr = 0x15;
+      cfg.pin_sda = I2C_SDA;
+      cfg.pin_scl = I2C_SCL;
+      cfg.freq = 400000;
 
       _touch_instance.config(cfg);
-      _panel_instance.setTouch(&_touch_instance); // タッチスクリーンをパネルにセットします。
+      _panel_instance.setTouch(&_touch_instance);
     }
 
-    setPanel(&_panel_instance); // 使用するパネルをセットします。
+    setPanel(&_panel_instance);
   }
 };
 
